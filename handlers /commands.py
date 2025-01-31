87b7bb18655b99fe5ca8b8139a99ed1ae353b4a0
Streamtg <202310475@issu.edu.do>
2025-01-30 20:59:57 -0400

    Update commands.py

--- handlers /commands.py
+++ handlers /commands.py
@@ -2,5 +2,5 @@ from pyrogram.types import Message
 
 async def start_command(client: Client, message: Message):
     await message.reply_text(
-        "¡Hola! Envíame un archivo y te generaré un enlace de descarga en Blogger 📁."
+        "¡Hola! Envíame un archivo y te generaré un enlace de descarga directa 📁.https://yoelmod.blogspot.com"
     )
